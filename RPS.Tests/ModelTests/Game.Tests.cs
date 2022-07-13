34922888fee88eed0211e41ec975140ad300258b
Alex Shevlin <alexshevlin1@gmail.com>
2022-07-13 09:52:09 -0700

    add test for displaying user input.

--- RPS.Tests/ModelTests/Game.Tests.cs
+++ RPS.Tests/ModelTests/Game.Tests.cs
@@ -1,4 +1,5 @@
-using Game;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RPS.Models;
 
 namespace RPS.Tests
 {
@@ -6,6 +7,13 @@ namespace RPS.Tests
   public class GameTests
   {
     [TestMethod]
-    public void 
+    public void ReturnUserInput_ReturnUserInputToNewGame_PlayerInfo()
+    {
+      string player1 = "rock";
+      string player2 = "paper";
+      string input = player1 + ", " + player2;
+      Game newGame = new Game(player1, player2);
+      Assert.AreEqual(input, newGame.ReturnUserInput());
+    }
   } 
 }
